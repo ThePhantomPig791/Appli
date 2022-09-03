083a4be0ed5e2edf8221dc072c37211026f25b9c
thephantompig791 <thephantompig791@gmail.com>
2022-09-03 09:45:25 -0400

    the funny "ready to power up your game" spin-off text

--- src/main/java/net/thephantompig791/appli/Appli.java
+++ src/main/java/net/thephantompig791/appli/Appli.java
@@ -1,15 +1,32 @@
 package net.thephantompig791.appli;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Appli implements ModInitializer {
 	public static final String MOD_ID = "appli";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static String VERSION = "";
+	public static int[] SEMVER;
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
+			VERSION = modContainer.getMetadata().getVersion().getFriendlyString();
+			if(VERSION.contains("+")) {
+				VERSION = VERSION.split("\\+")[0];
+			}
+			if(VERSION.contains("-")) {
+				VERSION = VERSION.split("-")[0];
+			}
+			String[] splitVersion = VERSION.split("\\.");
+			SEMVER = new int[splitVersion.length];
+			for(int i = 0; i < SEMVER.length; i++) {
+				SEMVER[i] = Integer.parseInt(splitVersion[i]);
+			}
+		});
+		LOGGER.info("Appli \" + VERSION + \" has initialized. Powering up your game that's already been powered up by many other (better) addons.");
 	}
 }
