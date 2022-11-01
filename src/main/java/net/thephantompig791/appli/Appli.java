525da40012315d87ea565cc265e73967d93dfd99
thephantompig791 <thephantompig791@gmail.com>
2022-11-01 16:57:49 -0400

    ---

--- src/main/java/net/thephantompig791/appli/Appli.java
+++ src/main/java/net/thephantompig791/appli/Appli.java
@@ -2,25 +2,38 @@ package net.thephantompig791.appli;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
+import net.thephantompig791.appli.power.factory.condition.AppliBlockConditions;
+import net.thephantompig791.appli.power.factory.condition.AppliEntityConditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Appli implements ModInitializer {
-	public static final String MOD_ID = "appli";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static String VERSION = "";
+public class Appli implements ModInitializer{
+    public static final String MOD_ID = "appli";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static String VERSION = "";
 
-	@Override
-	public void onInitialize() {
-		FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
-			VERSION = modContainer.getMetadata().getVersion().getFriendlyString();
-			if(VERSION.contains("+")) {
-				VERSION = VERSION.split("\\+")[0];
-			}
-			if(VERSION.contains("-")) {
-				VERSION = VERSION.split("-")[0];
-			}
-		});
-		LOGGER.info("Appli \" + VERSION + \" has initialized. Powering up your game that's already been powered up by many other (better) addons.");
-	}
+    @Override
+    public void onInitialize() {
+
+        AppliEntityConditions.register();
+        AppliBlockConditions.register();
+
+
+        //yeah, i took this from apoli.java, sorry about that, but like everyone else did, so...
+        FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
+            VERSION = modContainer.getMetadata().getVersion().getFriendlyString();
+            if(VERSION.contains("+")) {
+                VERSION = VERSION.split("\\+")[0];
+            }
+            if(VERSION.contains("-")) {
+                VERSION = VERSION.split("-")[0];
+            }
+        });
+        LOGGER.info("Appli " + VERSION + " has initialized. Ready to power up your game that's already been powered up by many other (better) power libraries.");
+    }
+
+    public static Identifier identifier(String path) {
+        return new Identifier(MOD_ID, path);
+    }
 }
