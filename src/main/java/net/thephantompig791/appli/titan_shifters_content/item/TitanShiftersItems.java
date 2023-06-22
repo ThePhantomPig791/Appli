78bae0d4fb37c678e97b8a01e5d3f147c643f2fa
ThePhantomPig791 <thephantompig791@gmail.com>
2023-06-21 23:22:42 -0400

    mfw titan injections for like the seventieth time

--- src/main/java/net/thephantompig791/appli/titan_shifters_content/item/TitanShiftersItems.java
+++ src/main/java/net/thephantompig791/appli/titan_shifters_content/item/TitanShiftersItems.java
@@ -15,14 +15,13 @@ public class TitanShiftersItems {
     public static final Item TITAN_SKIN_CROSSBOW = register("titan_skin_crossbow",
             new TitanSkinCrossbowItem(new FabricItemSettings()));
 
-    //I decided against using titan injections
-    /*
     public static final Item TITAN_INJECTION = register("titan_injection",
             new TitanSkinCrossbowItem(new FabricItemSettings()));
 
+    /*
     public static final Item EMPTY_INJECTION = register("empty_injection",
             new TitanSkinCrossbowItem(new FabricItemSettings()));
-     */
+    */
 
 
 
